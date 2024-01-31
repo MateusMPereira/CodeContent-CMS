02d9ff26c6b74a00def137200f638ec287cf93fb
MateusMPereira <mateusmichellipereira@gmail.com>
2024-01-31 03:16:54 -0300

    Update Joaquim.php

--- app/src/classes/Joaquim.php
+++ app/src/classes/Joaquim.php
@@ -20,4 +20,20 @@ class Joaquim
             exit(Julia::mergeDataView($view, $class->viewData));
         }
     }
+
+    public static function post(string $rota, array $props, string $viewName)
+    {
+        if (REQ_METHOD === 'POST' && matchRoute(ROTA, $rota)) {
+            $className = $props[0];
+            $method    = $props[1];
+
+            require_once SRC_PATH . '//controller//' . $className . '.php';
+            $class = new $className;
+            $class->$method();
+
+            $view = file_get_contents(SRC_PATH . '//view//' . $viewName . '.php');
+
+            exit(Julia::mergeDataView($view, $class->viewData));
+        }
+    }
 }
